ba4ed3f9401d489881d479adad0bc6b24a76249f
Olga Koval <ok@telemark-it.ru>
2020-04-04 22:35:53 +0300

    add webpack.config comments

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,10 @@ module.exports = {
   },
   devtool: `source-map`, // Подключаем sourcemaps
   devServer: { // Настраиваем сервер
-    contentBase: path.join(__dirname, `public`),
+    contentBase: path.join(__dirname, `public`), // Где искать сборку
+    // Автоматическая перезагрузка страницы
+    // По умолчанию приложение будет доступно по адресу http://localhost:8080
+    // Лучше открывать в режиме инкогнито, чтобы браузер не кэшировал файлы сборки
     watchContentBase: true,
   }
 };
