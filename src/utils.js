a390db4b10e1fdf5272ba92bb9860a194ba44f3a
Olga Koval <ok@telemark-it.ru>
2020-04-17 10:31:45 +0300

    add task esc event, add no-tasks component

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ export const RenderPosition = {
   BEFOREEND: `beforeend`
 };
 
-export const render = (container, element, place) => {
+export const render = (container, element, place = RenderPosition.BEFOREEND) => {
   switch (place) {
     case RenderPosition.AFTERBEGIN:
       container.prepend(element);
